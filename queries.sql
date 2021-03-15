567b512db1f9b6182d9fd6fe603b61870bd5c46e
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-15 14:27:36 -0400

    Sql Queries even stretch are complete

--- queries.sql
+++ queries.sql
@@ -2,16 +2,46 @@
 
 -- Find all customers with postal code 1010
 
+--SELECT  customername, postalcode FROM Customers 
+--WHERE postalcode = 1010
+
 -- Find the phone number for the supplier with the id 11
+
+-- SELECT suppliername, phone FROM [Suppliers]
+-- where supplierid = 11
 
 -- List first 10 orders placed, sorted descending by the order date
 
+-- SELECT * FROM [Orders]
+-- order by orderdate desc 
+-- limit 10
+
+
 -- Find all customers that live in London, Madrid, or Brazil
+
+-- SELECT * FROM [Customers]
+-- WHERE Country = 'London'
+-- OR Country = 'Madrid'
+-- OR Country = 'Brazil'
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
+-- INSERT into Customers(Country, ContactName, Address, City, PostalCode)
+-- VALUES('Middle Earth', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111'); 
+
+
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+
+-- update customers
+-- 	set postalcode = 11122
+-- where customerid = 92
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
+-- select count(distinct city) from customers
+
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+
+-- SELECT suppliername FROM [Suppliers] 
+-- where length(suppliername) >= 20
+
