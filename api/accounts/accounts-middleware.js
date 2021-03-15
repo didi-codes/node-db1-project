9df0648f8dbe098fba2cf5f1d59df83d787c7889
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-15 17:36:54 -0400

    checkAccountId middleware complete

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,5 +1,13 @@
+const Account = require('./accounts-model')
+
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  const { name, budget} = req.body
+
+  if(!name && !budget) {
+    res.status(400).json({
+      message: 'name and budget are required'
+    })
+  } else if(typeof name === 'string')
 }
 
 exports.checkAccountNameUnique = async (req, res, next) => {
@@ -7,5 +15,13 @@ exports.checkAccountNameUnique = async (req, res, next) => {
 }
 
 exports.checkAccountId = async (req, res, next) => {
-  // DO YOUR MAGIC
+  const { id } = req.params
+  const idExists = await Account.getById(id)
+  if(!idExists) {
+    res.status(404).json({
+      message: 'account not found'
+    })
+  } else {
+    next()
+  }
 }
