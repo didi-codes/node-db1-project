8173889d19960485bab1094cec235503d61c3693
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-15 19:01:30 -0400

    import router to server to test first endpoint on postman

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,8 +1,16 @@
+const express = require('express')
+const Account = require('./accounts-model')
+
 const router = require('express').Router()
 
 router.get('/', async (req, res, next) => {
-  // DO YOUR MAGIC
-})
+  try {
+    const data = await Account.getAll();
+    res.json(data);
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.get('/:id', (req, res, next) => {
   // DO YOUR MAGIC
--- api/server.js
+++ api/server.js
@@ -1,7 +1,15 @@
-const express = require("express");
+const express = require('express');
+const accountRouter = require('./accounts/accounts-router');
 
 const server = express();
 
 server.use(express.json());
 
+server.use('/api/accounts', accountRouter);
+
+server.get('/', (req, res) => {
+  res.status(200).json({
+    api: 'It Lives',
+  });
+});
 module.exports = server;
