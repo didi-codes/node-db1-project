6b42970886cb49ea10d6da7c8a55601577bb362d
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-15 18:27:26 -0400

    checkAccountPayload middleware completed

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,17 +1,35 @@
-const Account = require('./accounts-model')
+const Account = require('./accounts-model');
 
 exports.checkAccountPayload = (req, res, next) => {
-  const { name, budget} = req.body
+  const { name, budget } = req.body;
 
-  if(!name && !budget) {
+  if (!name && !budget) {
     res.status(400).json({
-      message: 'name and budget are required'
-    })
-  } else if(typeof name === 'string')
-}
+      message: 'name and budget are required',
+    });
+  } else if (typeof name === !'string') {
+    res.status(400).json({
+      message: 'name of account must be a string',
+    });
+  } else if (name.length < 3 || name.length > 100) {
+    res.status(400).json({
+      message: 'name of account must be between 3 and 100',
+    });
+  } else if (typeof budget === !'number') {
+    res.status(400).json({
+      message: 'budget of account must be a number',
+    });
+  } else if ((budget >= 0 || budget <= 1, 000, 000, 000)) {
+    res.status(400).json({
+      message: 'budget of account is too large or too small',
+    });
+  } else {
+    next();
+  }
+};
 
 exports.checkAccountNameUnique = async (req, res, next) => {
-  // DO YOUR MAGIC
+  
 }
 
 exports.checkAccountId = async (req, res, next) => {
